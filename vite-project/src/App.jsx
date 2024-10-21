cd8aa7da8e4d1d76c0b385c6839f223cf4d56453
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-21 14:53:00 +0300

    commits

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,16 +9,19 @@ function App() {
 
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState("");
+  const[loading, setLoading]= useState(false);
   const[error, setError]=useState("");
   const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 
   const fetchWeather = async(cityName)=>{
+    console.log("fetching weather for :", cityName)
     try{
       const response = await axios.get( `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
-      
+      console.log("API Response", response.data)
       if(response.status===200){
         setWeatherData(response.data);
+        setError("");
       }
       else{
         setError("weather data could not be recieved");
@@ -26,14 +29,15 @@ function App() {
     }
     catch(error){
       console.error("error fetching weather data",error);
-      setError('Failed to fetch weather data. Please try again later.');
-      
-    }
-    
+      setError('Failed to fetch weather data. Please try again later.'); 
+    } 
+     finally{
+      setLoading(false);
+     }
   };
 
   const handleSearch = () =>{
-    if(city){
+    if(city.trim()){
       fetchWeather(city);
     }
   };
@@ -43,14 +47,13 @@ function App() {
     <h1>Weather App</h1>
     <SearchBar setCity={setCity} city={city} handleSearch={handleSearch}/>
     {error && <p className="error">{error}</p>}
+    {loading && <p>Loading...</p>}
     {weatherData ? (
       <Weather data={weatherData}/>
     ) : ( !error && <p>Enter a city name to get the weather</p>)} 
-
    </div>
   );
 
 }
-
 
 export default App;
